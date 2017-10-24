ac396f36cc6b1776c7583dbd49b4080edffba4e8
Ori71e2 <Ori71e2@gmail.com>
2017-10-24 12:48:10 +0800

    add install

--- apps/ip_proxy_poll/views.py
+++ apps/ip_proxy_poll/views.py
@@ -3,9 +3,18 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import imp
+import os
 # Create your views here.
 
 
 def install(void):
+    modles = [requests, chardet, web.py, gevent, psutil]
+    response = ['str':'', ]
+    for each in modules:
+        try:
+            imp.find_module(each)
+        except ImportError:
+            if os.system(each) == 0:
+                response[str] += ',' + each
     return HttpResponse("Success!")
