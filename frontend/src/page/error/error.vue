8d9347becdd29e5d77137f0e9cfef85f94782487
Ori71e2 <Ori71e2@gmail.com>
2018-03-14 22:43:17 +0800

    add

---
+++ frontend/src/page/error/error.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="error-page">
+       <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
---
+++ frontend/src/store/mutations.js
@@ -0,0 +1,17 @@
+import Cookies from 'js-cookie'
+import i18n from '@/util/i18n'
+
+export default {
+    changeLang(state, data){
+        if(data){
+            Cookies.set('lang', data)
+            i18n.locale = data
+            state.lang = data
+        }   
+    },
+    changeLayout(state, data){
+        if(data){
+            state.navbarPosition = data
+        }
+    }
+}
