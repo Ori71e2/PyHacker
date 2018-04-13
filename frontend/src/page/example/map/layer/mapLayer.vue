19226e828920de7d67e75e8e41113c9db2d347b9
Ori71e2 <Ori71e2@gmail.com>
2018-04-13 23:00:59 +0800

    add

--- frontend/src/page/example/map/layer/mapLayer.vue
+++ frontend/src/page/example/map/layer/mapLayer.vue
@@ -23,40 +23,8 @@ export default {
             })
             var eqLayer = new EqLayer(map, AMap);
             eqLayer.layer.setMap(map);
-            //eqLayer.layer.render =  eqLayer.onRender();
 
-/*
-          
-            for (var i=0; i<3; i++) {
-                canvas[i]= document.createElement('canvas');
-                canvas[i].width = map.getSize().width;
-                canvas[i].height = map.getSize().height;
-            }
-            [eqCanvas, dyCanvas, staCanvas] = canvas;
-           
-            var eqLayer = new AMap.CustomLayer(eqCanvas, {
-                        zooms: [3,20],
-                        zIndex: 12
-                    });
-            eqLayer.setMap(map);
-            eqLayer.render = eqOnRender;
-
-            var eqCtx = eqCanvas.getContext('2d');
-            var pos, started
-            function eqOnRender() {
-                pos = map.lngLatToContainer([118.716184,33.720615]);
-                eqDraw();
-
-            }
-            function eqDraw() {
-                eqCtx.clearRect(0, 0, eqCanvas.width, eqCanvas.height);
-                eqCtx.beginPath();
-                eqCtx.arc(pos.x, pos.y, 50, 0, 2*Math.PI);
-                eqCtx.stroke();
-            }
-       
-*/
-
+  
             
         }
     }
