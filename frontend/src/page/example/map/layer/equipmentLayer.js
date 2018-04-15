9e5ded9d9060b567cc5541e9456561f33a267824
Ori71e2 <Ori71e2@gmail.com>
2018-04-15 23:02:21 +0800

    add

--- frontend/src/page/example/map/layer/equipmentLayer.js
+++ frontend/src/page/example/map/layer/equipmentLayer.js
@@ -12,23 +12,40 @@ class EqLayer {
                             zooms: [3, 20],
                             zIndex: 12
                         });
-        //this.svgDraw = SVG(this.svg).size(this.map.getSize().width, this.map.getSize().height);
         this.svgDraw = SVG(this.svg).size(this.width, this.height);
         this.layer.render =  this.onRender();
         this.layer.setMap(map);
+        this.posArray = [[118.720366, 33.69942], [118.720623, 33.70349], [118.721053, 33.707703], [118.720967, 33.713343], [118.721224, 33.718769], [118.71719, 33.719412], [118.715044, 33.717484]];
+        Array.prototype.GpsToCo = function(map) {
+            var arr = [];
+            for(var i=0; i<this.length; i++) {
+                var pos = map.lngLatToContainer(this[i]);
+                arr.push([pos.x, pos.y]);
+            }
+            return arr;
+        }
     }
     draw() {
-        //this.pos = this.map.lngLatToContainer([118.716184,33.720615]);
-        this.pos = this.map.lngLatToContainer([118.716184,33.720615]);
+        var pos = this.map.lngLatToContainer([118.716184,33.720615]);
         this.svgDraw.clear();
-        var x = this.pos.x;
-        var y = this.pos.y;
-        this.svgDraw.line(x, y, x+100, y+50).stroke({width:1});
+        //var x = this.pos.x;
+        //var y = this.pos.y;
+        this.svgDraw.polyline(this.posArray.GpsToCo(this.map)).fill('none').stroke({width:1});
+        var rect = this.svgDraw.rect(50, 50).x(pos.x).y(pos.y);
+        rect.click(this.click());
+        //rect.fire('mouseup');
+        //rect.on('mouseup', this.click());
     }
 
     onRender() { 
         return () => {
             this.draw();    
+        }
+    }
+
+    click() {
+        return () => {
+            alert('PolyLine');
         }
     }
 }
