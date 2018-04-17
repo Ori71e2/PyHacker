4b87da22b94f2b3748664b79f0c290746792fe1a
Ori71e2 <Ori71e2@gmail.com>
2018-04-17 20:02:46 +0800

    add

--- frontend/src/page/example/map/layer/equipmentDraw.js
+++ frontend/src/page/example/map/layer/equipmentDraw.js
@@ -1,6 +1,7 @@
 'use strict';
 class EqDraw {
     constructor(map, AMap) {
+        /*
         this.lineArr = [[118.720366, 33.69942], [118.720623, 33.70349], [118.721053, 33.707703], [118.720967, 33.713343], [118.721224, 33.718769], [118.71719, 33.719412], [118.715044, 33.717484]];
         this.polyline = new AMap.Polyline({
             path: this.lineArr,
@@ -10,9 +11,24 @@ class EqDraw {
             strokeStyle: "solid",
             strokeDasharray: [10, 5]
         });
-        this.polyline.setMap(map);
+        */
+        store.dispatch('equipment/getLine').then(res => {
+            this.polyline = new AMap.Polyline({
+                path: res.data,
+                strokeColor: "#3366FF",
+                strokeOpacity: 1,
+                strokeWeight: 5,
+                strokeStyle: "solid",
+                strokeDasharray: [10, 5]
+            });
+            this.polyline.setMap(map);
+            this.polyline.on('click', function(){alert("Over")});
+        }).catch(() => {
+            console.log('无法获取数据')
+        })
+        //this.polyline.setMap(map);
         //this.polyline.on('mouseover', function(){alert("Over")});
-        this.polyline.on('click', function(){alert("Over")});
+        //this.polyline.on('click', function(){alert("Over")});
     }
     draw() {
         this.pos = this.map.lngLatToContainer([118.716184,33.720615]);
